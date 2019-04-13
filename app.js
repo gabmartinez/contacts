a22d213eeaa6a992b5ebcf9d6e13495026e2f296
Gabriel L Martinez <g.lorenzo.martinez@gmail.com>
2019-04-12 21:20:09 -0400

    Create auth section

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var users = require('./controllers/users')
 
 const app = express()
 app.set('view engine', 'pug')
@@ -7,6 +8,7 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
     res.render('index', { title: 'My Contacts', message: 'Hello World!' })
 })
+app.use('/auth', users)
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`App is running on port ${server.address().port}!`);
---
+++ controllers/users.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var firebase = require("firebase")
+const config = require('../credential')
+
+var router = express.Router()
+firebase.initializeApp(config)
+const auth = firebase.auth()
+
+router.get('/', (req, res) => {
+    res.render('login', { section: 'My Contact'})
+})
+
+router.post('/login', (req, res) => {
+    auth.signInWithEmailAndPassword(req.body.email, req.body.password)
+    .then((userCredential) => {
+        res.redirect('/')
+    }).catch((error) => {
+        res.render('login', { section: 'Login', message: error.message })
+    });
+})
+
+router.get('/logout', (req, res) => {
+    auth.signOut().then(() => {
+        res.redirect(req.baseUrl + '/')
+    }).catch((error) => {
+      res.status(500).render("Something Wrong!")
+    })
+})
+
+module.exports = router
